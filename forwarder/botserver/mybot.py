0a40c5d7eb61d0f2c63f0781a55335f714362ccf
Gautam Ajey <gautamajey@gmail.com>
2025-12-26 00:02:45 -0500

    corrected mybot.py

--- forwarder/botserver/mybot.py
+++ forwarder/botserver/mybot.py
@@ -24,7 +24,7 @@ def send_to_apiserver(name, username, chatId, active):
 @bot.message_handler(commands=['stopbot', 'delete'])
 def accept_stop(message):
   name = "%s %s" % (message.chat.first_name, message.chat.last_name)
-  LOGGER.info("Command(stopbot/delete) - %s: %s ==> %d", (name, message.chat.username, message.chat.id))
+  LOGGER.info("Command(stopbot/delete) - %s: %s ==> %d", name, message.chat.username, message.chat.id)
   updateTelegramUser(name, message.chat.username, message.chat.id, False)
   send_to_apiserver(name, message.chat.username, message.chat.id, False)
   bot.reply_to(message, "%s, will not send message until /start command - %s" % (name, botCfg.VERSION))
@@ -33,7 +33,7 @@ def accept_stop(message):
 @bot.message_handler(commands=['start'])
 def accept_start(message):
   name = "%s %s" % (message.chat.first_name, message.chat.last_name)
-  LOGGER.info("Comand(start) - %s: %s ==> %d", (name, message.chat.username, message.chat.id))
+  LOGGER.info("Comand(start) - %s: %s ==> %d", name, message.chat.username, message.chat.id)
   createTelegramUser(name, message.chat.username, message.chat.id, True)
   send_to_apiserver(name, message.chat.username, message.chat.id, True)
   bot.reply_to(message, "%s, will send messages until /stopbot command - %s" % (name, botCfg.VERSION))
@@ -42,7 +42,7 @@ def accept_start(message):
 @bot.message_handler(commands=['help'])
 def accept_help(message):
   name = "%s %s" % (message.chat.first_name, message.chat.last_name)
-  LOGGER.info("Comand(help) - %s: %s ==> %d", (name, message.chat.username, message.chat.id))
+  LOGGER.info("Comand(help) - %s: %s ==> %d", name, message.chat.username, message.chat.id)
   msg = """
     
     %s
@@ -60,7 +60,7 @@ def accept_help(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
   name = "%s %s" % (message.chat.first_name, message.chat.last_name)
-  LOGGER.info("Other Messages - %s: %s ==> %d", (name, message.chat.username, message.chat.id))
+  LOGGER.info("Other Messages - %s: %s ==> %d", name, message.chat.username, message.chat.id)
   msg = "'gaksmsbot' is a bot to receive forwarded messages, for details send /help - %s" % botCfg.VERSION
   bot.reply_to(message, msg)
 
